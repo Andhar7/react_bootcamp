3740b617a035cbaf703f2729031b8b1b5f6dfef2
Andhar7 <boxingprofi@web.de>
2025-11-30 12:47:07 +0100

    secon task added

---
+++ 01-starting-project/src/tasks/second/AppThird.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import Todo from './Todo'
+import './styles.css';
+
+// don't remove the export keyword here!
+export const DUMMY_TODOS = [
+    'Learn React',
+    'Practice React',
+    'Profit!'
+];
+
+// don't change the Component name "App"
+export default function App() {
+
+    return (
+        <ul>
+            {DUMMY_TODOS.map((index) => (
+                <Todo words={index}/>
+            ))}
+        </ul>
+    );
+}
